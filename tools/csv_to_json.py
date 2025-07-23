c3634ce91a730db5dedc41754eff1bdc5fb4f312
Tom Casavant <tfcasavant@gmail.com>
2025-07-23 10:11:42 -0400

    Added sample website, json converter and README

---
+++ tools/csv_to_json.py
@@ -0,0 +1,9 @@
+import csv
+import json
+
+with open("ohio.csv", encoding="utf-8-sig") as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+
+with open("data.json", "w", encoding="utf-8") as jsonfile:
+    json.dump(data, jsonfile, indent=2)
